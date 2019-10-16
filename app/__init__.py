8403d5608bc79698afd50cf7259a542e52c92a64
Bailey Middendorf <bam95899@bethel.edu>
2019-10-16 13:27:45 -0500

    fix sentry import

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from datetime import datetime
 
 from flask import Flask, render_template, make_response, redirect, session
 
+import sentry_sdk
+
 app = Flask(__name__)
 app.config.from_object('config')
 app.url_map.strict_slashes = False
 
 if app.config['SENTRY_URL']:
-    import sentry_sdk
     from sentry_sdk.integrations.flask import FlaskIntegration
     sentry_sdk.init(dsn=app.config['SENTRY_URL'], integrations=[FlaskIntegration()])
 
